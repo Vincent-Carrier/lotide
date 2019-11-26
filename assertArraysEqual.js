b2a20092ca89b343b82e3f42da2a889a3c5a8d64
Vincent-Carrier <vincentcarrier15@gmail.com>
2019-11-26 18:09:06 -0500

    Add assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false
+  for (let [i, e] of arr1.entries()) {
+    if (e !== arr2[i]) return false
+  }
+  return true
+}
+
+function assertArraysEqual(actual, expected) {
+  const condition = eqArrays(actual, expected)
+  if (condition) {
+    console.log(`🛑 Assertion Failed: "${actual} !== ${expected}"`);
+  } else {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+}
