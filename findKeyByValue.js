9a8f231b62ad0f708458f5996af9a88af10e3bcc
Vincent-Carrier <vincentcarrier15@gmail.com>
2019-11-27 16:10:47 -0500

    Add findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: "${actual} !== ${expected}"`);
+  }
+};
+
+const findKeyByValue = (obj, val) => Object.keys(obj).find(s => obj[s] === val)
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
