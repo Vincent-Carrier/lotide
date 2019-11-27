29758c5a97f1476fe014a62f51632eb768a87448
Vincent-Carrier <vincentcarrier15@gmail.com>
2019-11-27 17:18:36 -0500

    Fix assertObjectEqual display

--- assertObjectEqual.js
+++ assertObjectEqual.js
@@ -1,9 +1,13 @@
+const inspect = require('util').inspect;
+
 function assertObjectsEqual(actual, expected) {
   const condition = eqObjects(actual, expected)
   if (condition) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🛑 Assertion Failed: "${actual} !== ${expected}"`);
+    console.log(`🛑 Assertion Failed: "${inspect(actual)} !== ${inspect(expected)}"`);
   }
 }
+
+assertObjectsEqual({name: "Vincent"}, {name: "Vincent"});
 
