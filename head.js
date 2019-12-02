dba160c2d3d99871307fd4c4d74abeab113a376e
Vincent-Carrier <vincentcarrier15@gmail.com>
2019-12-02 10:43:32 -0500

    Refactor assertEqual and head to use modules

--- head.js
+++ head.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  const condition = actual === expected;
-  if (condition) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: "${actual} !== ${expected}"`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 function head(arr) {
   if (!arr || arr === []) return undefined;
   return arr[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
